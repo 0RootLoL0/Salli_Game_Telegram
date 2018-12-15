eb4ba8ea7499cb312c4f02609ae31aac25eeadce
Your Name <you@example.com>
2018-12-15 15:20:36 +0300

    7

--- main.py
+++ main.py
@@ -9,14 +9,13 @@ def send_welcome(message):
 	bot.send_message(message.chat.id, "добро пожаловать")
 	bot.send_message(message.chat.id, "Для осмотра состояния нажми 9")
 	bot.send_message(message.chat.id, "для начала отправте любую цыфру")
-	user_m[message.chat.id] = {"nickname": message.chat.username, "hard": 10, "hangree": 10,
-									  "root_scena": 0, "schena": 0}
-	print(user_m)
+	user_m[message.chat.id] = {"nickname": message.chat.username, "hard": 10, "hangree": 10,"root_scena": 0, "schena": 0}
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	print(message.chat.id)
-	print(message.text)
-	bot.send_message(message.chat.id, message.text)
+    if user_m.get(message.chat.id) != None:
+        bot.send_message(message.chat.id, message.text)
+    else:
+        bot.send_message(message.chat.id, "вы не зарегистриваны")
 
 bot.polling()
