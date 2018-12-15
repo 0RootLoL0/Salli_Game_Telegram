4559576404ee8ee163c4fc519235da0f6e3ee81c
Your Name <you@example.com>
2018-12-15 19:38:54 +0300

    создаём оформление

--- main.py
+++ main.py
@@ -138,14 +138,14 @@ user_m = {}
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-  bot.send_message(message.chat.id, ":'######:::::'###::::'##:::::::'##:::::::'####:"+
-                                    ":##... ##:::'## ##::: ##::::::: ##:::::::. ##::"+
-                                    ":##:::..:::'##:. ##:: ##::::::: ##:::::::: ##::"+
-                                    ": ######::'##:::. ##: ##::::::: ##:::::::: ##::"+
-                                    ":..... ##: #########: ##::::::: ##:::::::: ##::"+
-                                    "'##::: ##: ##.... ##: ##::::::: ##:::::::: ##::"+
-                                    ". ######:: ##:::: ##: ########: ########:'####:"+
-                                    ":......:::..:::::..::........::........::....::")
+  bot.send_message(message.chat.id, ":'######:::::'###::::'##:::::::'##:::::::'####:")
+  bot.send_message(message.chat.id, ":##... ##:::'## ##::: ##::::::: ##:::::::. ##::")
+  bot.send_message(message.chat.id, ":##:::..:::'##:. ##:: ##::::::: ##:::::::: ##::")
+  bot.send_message(message.chat.id, ": ######::'##:::. ##: ##::::::: ##:::::::: ##::")
+  bot.send_message(message.chat.id, ":..... ##: #########: ##::::::: ##:::::::: ##::")
+  bot.send_message(message.chat.id, "'##::: ##: ##.... ##: ##::::::: ##:::::::: ##::")
+  bot.send_message(message.chat.id, ". ######:: ##:::: ##: ########: ########:'####:")
+  bot.send_message(message.chat.id, ":......:::..:::::..::........::........::....::")
   bot.send_message(message.chat.id, "добро пожаловать")
   bot.send_message(message.chat.id, "Для начала нажмит")
   bot.send_message(message.chat.id, textMess[0][0]["text"])
