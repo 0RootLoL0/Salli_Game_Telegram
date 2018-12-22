1a46746c7181148d9cba60d4e9bfecb63d05612d
Your Name <you@example.com>
2018-12-22 12:52:55 +0300

    db include fix 8

--- sql.py
+++ sql.py
@@ -1,8 +1,8 @@
 import sys
 import json
-import sqlite3
+import psycopg2
 
-conn = sqlite3.connect('chat.sqlite')
+conn = psycopg2.connect(dbname='ddsn9cpuvta6n6', user='ishjjypgrfjrlt', password='36d2511ed5ed0f50e436d37db05e690ed3499ce7168d9a12965cc264942b2fbd', host='ec2-107-20-237-78.compute-1.amazonaws.com')
 cursor = conn.cursor()
 
 
@@ -10,7 +10,7 @@ if sys.argv[1] == "0":
     try:
         cursor.execute(sys.argv[2])
         print(200)
-    except sqlite3.OperationalError:
+    except psycopg2.OperationalError:
         print(500)
 
 elif sys.argv[1] == "1":
@@ -18,7 +18,7 @@ elif sys.argv[1] == "1":
         cursor.execute(sys.argv[2])
         results = cursor.fetchall()
         print(json.dumps(results))
-    except sqlite3.OperationalError:
+    except psycopg2.OperationalError:
         print(500)
 
 conn.commit()
